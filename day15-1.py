c4bb11d2c6c01e684acc40f6cba01a2d1a2f2673
dangerworm <dangerworm@gmail.com>
2017-12-18 01:09:31 +0000

    Finally got day 15 optimised and done.

--- day15-1.py
+++ day15-1.py
@@ -1,41 +1,32 @@
 class Generator:
-    def __init__(self, startingValue, factor):
-        self.startingValue = startingValue
-        self.previousValue = startingValue
-        self.factor = factor
-        self.divisor = 2147483647
+    def __init__(self, start, multiplier):
+        self.value = start
+        self.multiplier = multiplier
 
     def getNext(self):
-        product = self.previousValue * self.factor
-        remainder = product % self.divisor
-        self.previousValue = product
-        return remainder
+        self.value = (self.value * self.multiplier) % 2147483647
+        return self.value
+
+# Initial values
+a, b = 277, 349 # Puzzle input
+#a, b = 65, 8921 # Test
 
 a = Generator(65, 16807)
 b = Generator(8921, 48271)
 
 judgeTotal = 0
-#n = 5
+#n = 50
 n = 40000000
 i = 0
 while i < n:
-    if i % 1000 == 0:
-        print i
-    
-    aNext, bNext = a.getNext(), b.getNext() 
-    #print '%16d %16d' % (aNext, bNext)
+    if i % 1000000 == 0:
+        print '%dM' % (i/1000000)
 
-    if aNext == a.startingValue:
-        print 'a reached its starting value'
-        break
-    if bNext == b.startingValue:
-        print 'b reached its starting value'
-        break
-
-    n1 = bin(aNext)[-16:]
-    n2 = bin(bNext)[-16:]
-
-    if n1 == n2:
+    #Remainders
+    aR = int(bin(a.getNext())[2:][-16:], 2)
+    bR = int(bin(b.getNext())[2:][-16:], 2)
+        
+    if not aR ^ bR:
         judgeTotal += 1
 
     i += 1
---
+++ day15-2.py
@@ -0,0 +1,41 @@
+from math import log
+
+class Generator:
+    def __init__(self, start, multiplier, divisor):
+        self.value = start
+        self.multiplier = multiplier
+        self.divisor = divisor
+
+    def getNext(self):
+        self.value = (self.value * self.multiplier) % 2147483647
+        while self.value % self.divisor != 0:
+            self.value = (self.value * self.multiplier) % 2147483647
+        return self.value
+
+# Initial values
+a, b = 277, 349 # Puzzle input
+#a, b = 65, 8921 # Test
+
+a = Generator(a, 16807, 4)
+b = Generator(b, 48271, 8)
+
+judgeTotal = 0
+#n = 5
+n = 5000000
+i = 0
+while i < n:
+    if i % 100000 == 0:
+        print '%d' % i
+
+    #Remainders
+    x = a.getNext()
+    y = b.getNext()
+    aR = int(bin(x)[2:][-16:], 2)
+    bR = int(bin(y)[2:][-16:], 2)
+    
+    if not aR ^ bR:
+        judgeTotal += 1
+
+    i += 1
+    
+print judgeTotal
