5c9e9aca2f9d580d5a34e3238dd1828d4cf7e66e
dangerworm <dangerworm@gmail.com>
2017-12-21 02:11:19 +0000

    Solved first part of day 18 and both parts of day 19. Still working on 18 part 2.

---
+++ day18-1.py
@@ -0,0 +1,113 @@
+class Interpreter:
+    def __init__(self):
+        self.freq = 0
+        self.counter = 0
+        self.instructions = {}
+        self.registers = {}
+
+    def addInstruction(self, i, instruction):
+        self.instructions[i] = instruction
+
+    def snd(self, x):
+        if not self.isNumeric(x):
+            x = self.registers[x]
+        self.freq = x
+
+    def set(self, x, y):
+        if x not in self.registers:
+            self.registers[x] = 0
+
+        if not self.isNumeric(y):
+            y = self.registers[y]
+            
+        self.registers[x] = y
+
+    def add(self, x, y):
+        if x not in self.registers:
+            self.registers[x] = 0
+
+        if not self.isNumeric(y):
+            y = self.registers[y]
+            
+        self.registers[x] += y
+
+    def mul(self, x, y):
+        if x not in self.registers:
+            self.registers[x] = 0
+
+        if not self.isNumeric(y):
+            y = self.registers[y]
+                
+        self.registers[x] *= y
+
+    def mod(self, x, y):
+        if x not in self.registers:
+            self.registers[x] = 0
+
+        if not self.isNumeric(y):
+            y = self.registers[y]
+
+        self.registers[x] %= y
+
+    def rcv(self, x):
+        if not self.isNumeric(x):
+            x = self.registers[x]
+        
+        if x == 0:
+            pass
+        else:
+            return 'Recovered', self.freq
+
+    def jgz(self, x, y):
+        if not self.isNumeric(x):
+            x = self.registers[x]
+
+        if x > 0:
+            self.counter += y - 1
+
+    def isNumeric(self, x):
+        checkValue = str(x)
+        if checkValue[0] == '-':
+            checkValue = checkValue[1:]
+
+        value = eval('u"%s"' % checkValue)
+        return value.isnumeric()
+
+    def param(self, value):
+        checkValue = value
+        if value[0] == '-':
+            checkValue = value[1:]
+
+        if not eval('u"' + checkValue + '"').isnumeric():
+            return '\'%s\'' % value
+
+        return value
+
+    def getNextCommand(self):
+        i = self.instructions[self.counter]
+
+        command = 'cpu.%s(%s' % (i[0], cpu.param(i[1]))
+        if len(i) > 2:
+            command += ', %s' % cpu.param(i[2])
+        command += ')'
+
+        self.counter += 1
+
+        return command
+
+cpu = Interpreter()
+
+data = file('day18-data.txt', 'r').read().split('\n')
+
+for i in range(len(data)):
+    cpu.addInstruction(i, data[i].split(' '))
+
+output = None
+while output == None:
+    command = cpu.getNextCommand()
+
+    #print command
+    output = eval(command)
+    #print cpu.registers, output
+   
+print output
---
+++ day19-2.py
@@ -0,0 +1,51 @@
+route = file('day19-data.txt','r').read().split('\n')
+route = [list(line) for line in route]
+
+U, R, D, L  = 0, 1, 2, 3
+
+d = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+
+x, y = route[0].index('|'), 0
+heading = D
+
+steps = 0
+letters = ''
+ignore = ['|','-','+']
+
+while True:
+    c = route[y][x]
+
+    if c == ' ':
+        break
+
+    if c not in ignore:
+        letters += c
+    
+    if route[y][x] != '+':
+        x += d[heading][0]
+        y += d[heading][1]
+    else:
+        up = [x, y-1]
+        down = [x, y+1]
+        left = [x-1, y]
+        right = [x+1, y]
+
+        backwards = d[(heading + 2) % 4]
+
+        surroundings = []
+        for direction in d:
+            if direction[0] == backwards[0] and direction[1] == backwards[1]:
+                surroundings.append(' ')
+            else:
+                s = route[y + direction[1]][x + direction[0]]
+                surroundings.append(s)
+
+        #print [i for i in range(4) if surroundings[i] != ' ']
+        heading = [i for i in range(4) if surroundings[i] != ' '][0]
+
+        x += d[heading][0]
+        y += d[heading][1]
+
+    steps += 1
+    
+print steps
